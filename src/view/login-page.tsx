3edc183aaac840dce34910154cfc424473bc45ed
l-junpu <leejunpu97@gmail.com>
2024-10-13 23:00:43 +0800

    Updated login page

--- src/view/login-page.tsx
+++ src/view/login-page.tsx
@@ -1,16 +1,16 @@
 import "./login-page.css";
+import "react-toastify/dist/ReactToastify.css";
 
 import { useState } from "react";
 import { FaLock, FaUser } from "react-icons/fa";
 
 import FetchAPI from "../api/helper";
 import { useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 
 const LoginPageView = () => {
   const navigate = useNavigate();
 
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
   const [register, setRegister] = useState(false);
 
   const [username, setUsername] = useState("");
@@ -19,7 +19,6 @@ const LoginPageView = () => {
 
   const handleLoginOrRegister = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setSuccess("");
 
     var api: string = "";
 
@@ -41,20 +40,20 @@ const LoginPageView = () => {
     });
 
     if (response == null) {
-      setError("Unable to connect to server");
+      toast.error("Unable to connect to server");
+      return;
     }
 
     const status: boolean = response;
 
     if (!status) {
-      setError("Incorrect username or password");
+      toast.error("Incorrect username or password");
       return;
     }
 
-    setError("");
     if (register) {
-      setSuccess("Successfully registered");
-      setRegister(false);
+      handleToggleLoginRegister(false);
+      toast.success("Successfully registered");
       return;
     }
 
@@ -63,54 +62,55 @@ const LoginPageView = () => {
     navigate("/dashboard/tasks", { replace: true });
   };
 
+  const handleToggleLoginRegister = (status: boolean) => {
+    setUsername("");
+    setPassword("");
+    setConfirmPassword("");
+    setRegister(status);
+  };
+
   return (
-    // Container for the entire login or registration form
-    <div className="login-container">
-      {/* Form element that handles submission with the handleLoginOrRegister function */}
-      <form onSubmit={(e) => handleLoginOrRegister(e)}>
-        {/* Header that dynamically changes between "Register" and "Login" based on the register state */}
-        <h1>{register ? "Register" : "Login"}</h1>
-        {/* Input field for the username */}
-        <div className="login-input-box">
-          <input name="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required />
-          <FaUser className="login-icon" />
-        </div>
-        {/* Input field for the password */}
-        <div className="login-input-box">
-          <input name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
-          <FaLock className="login-icon" />
-        </div>
-        {/* Conditional input field for confirming the password during registration */}
-        {register && (
+    <>
+      <ToastContainer position="bottom-right" />
+      {/* Container for the entire login or registration form */}
+      <div className="login-container">
+        {/* Form element that handles submission with the handleLoginOrRegister function */}
+        <form onSubmit={(e) => handleLoginOrRegister(e)}>
+          {/* Header that dynamically changes between "Register" and "Login" based on the register state */}
+          <h1>{register ? "Register" : "Login"}</h1>
+          {/* Input field for the username */}
           <div className="login-input-box">
-            <input
-              name="confirm"
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              placeholder="Confirm Password"
-              required
-            />
+            <input name="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required />
+            <FaUser className="login-icon" />
+          </div>
+          {/* Input field for the password */}
+          <div className="login-input-box">
+            <input name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
             <FaLock className="login-icon" />
           </div>
-        )}
-        {/* Submit button with dynamic text based on the register state */}
-        <button type="submit">{register ? "Register" : "Login"}</button>
-        {/* Display error messages */}
-        <div className="error-message">{error}</div>
-        {/* Display success messages */}
-        <div className="success-message">{success}</div>
-        {/* Link to toggle between login and registration */}
-        <div className="register-link">
-          <p>
-            {register ? "Already have an account? " : "Don't have an account? "}
-            <a href="#" onClick={() => setRegister(!register)}>
-              {register ? "Login" : "Register"}
-            </a>
-          </p>
-        </div>
-      </form>
-    </div>
+          {/* Conditional input field for confirming the password during registration */}
+          {register && (
+            <div className="login-input-box">
+              <input name="confirm" type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Confirm Password" required />
+              <FaLock className="login-icon" />
+            </div>
+          )}
+          {/* Submit button with dynamic text based on the register state */}
+          <button type="submit" className="login-button">
+            {register ? "Register" : "Login"}
+          </button>
+          {/* Link to toggle between login and registration */}
+          <div className="register-link">
+            <p>
+              {register ? "Already have an account? " : "Don't have an account? "}
+              <a href="#" onClick={() => handleToggleLoginRegister(!register)}>
+                {register ? "Login" : "Register"}
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
