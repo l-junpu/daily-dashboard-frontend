77a89aa0aeba1a05e22a8167738c39406d48a953
zhitern <zhitern@DESKTOP-097VIPS>
2024-11-19 17:03:56 +0800

    Implement virtualization on chat history and chat

--- src/base-component/virtualized-list/virtualized-list.tsx
+++ src/base-component/virtualized-list/virtualized-list.tsx
@@ -1,6 +1,3 @@
-import { useRef } from "react";
-
-// import { List, AutoSizer, CellMeasurer, CellMeasurerCache } from 'react-virtualized';
 import { Virtuoso } from "react-virtuoso";
 
 interface VirtualizedListProps {
@@ -9,39 +6,12 @@ interface VirtualizedListProps {
   }
 
 const VirtualizedList = ({rowCount, rowComponent} : VirtualizedListProps) => {
-    // const itemCache = useRef(new CellMeasurerCache({
-    //     fixedHeight: true,
-    //     defaultHeight: 50
-    //   }))
 
     return (
         <Virtuoso 
         totalCount={rowCount}
         itemContent={index => rowComponent(index)}
         />
-        // <AutoSizer>
-        //       {({height, width}) => (
-        //         <List
-        //         height={height}
-        //         width={width}
-        //         rowHeight={itemCache.current.rowHeight}
-        //         deferredMeasurementCache={itemCache.current}
-        //         rowCount={rowCount}
-        //         rowRenderer={(
-        //           { key, index, parent }) => (
-        //             <CellMeasurer 
-        //             key={key} 
-        //             cache={itemCache.current}
-        //             parent={parent}
-        //             columnIndex={0}
-        //             rowIndex={index}
-        //             >
-        //             <div></div>
-        //             </CellMeasurer>
-        //         )}
-        //       />
-        //       )}
-        //     </AutoSizer>
     );
 };
 
--- src/view/llm-dashboard.tsx
+++ src/view/llm-dashboard.tsx
@@ -37,58 +37,11 @@ const LLMDashboardView = () => {
   const [createChat, setCreateChat] = useState<boolean>(false);
 
   // Conversation Data
-  const [titles, setTitles] = useState<string[]>([
-    "this is a conversation lolasdasdasdasd",
-    "this is a conversation",
-    "this is a",
-    "this is a",
-    "this is a",
-    "this is a",
-    "this is a",
-    "this is a",
-    "this is a",
-    "this is a",
-    "this is a",
-    "this is a",
-    "this is a",
-    "this is a",
-    "this is a",
-  ]);
+  const [titles, setTitles] = useState<string[]>([]);
   const [activeTitle, setActiveTitle] = useState("");
   const [currentPrompt, setCurrentPrompt] = useState("");
   const [awaitingResponse, setAwaitingResponse] = useState(false);
-  const [messages, setMessages] = useState<Message[]>([
-    {
-      role: "user",
-      contents:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker",
-    },
-    {
-      role: "assistant",
-      contents:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-    },
-    {
-      role: "user",
-      contents:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker",
-    },
-    {
-      role: "assistant",
-      contents:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-    },
-    {
-      role: "user",
-      contents:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker",
-    },
-    {
-      role: "assistant",
-      contents:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-    },
-  ]);
+  const [messages, setMessages] = useState<Message[]>([]);
 
   // Active Buttons
   const secondaryButtonProps: ButtonProps[] = [
@@ -194,12 +147,14 @@ const LLMDashboardView = () => {
           <div className="conversation-container">
             {/* Actual Chat Area - 50% Subset of Entire Convo Area */}
             <div className="chat-zone">
-              {messages.map((message, index) => (
-                <p key={index} className={message.role == "user" ? "chat" : "chat user"}>
-                  {message.contents}
+              <VirtualizedList 
+              rowCount={messages.length}
+              rowComponent={(index) => (
+                <p key={index} className={messages[index].role == "user" ? "chat" : "chat user"}>
+                  {messages[index].contents}
                 </p>
-              ))}
-              <div ref={endRef} />
+              )}
+              />
             </div>
           </div>
           <div className="footer">
